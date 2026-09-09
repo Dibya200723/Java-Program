09ea63777b8e3279509766ab3935a3de0698af97
Dibya200723 <mndibya7@gmail.com>
2026-09-09 10:23:14 +0530

    Updated program

--- Student2.java
+++ Student2.java
@@ -1,8 +1,11 @@
-public class Student {
+public class Student2 {
     int id;
     String name;
+
+}
+class Student3{
     public static void main(String[] args) {
-        Student s1=new Student();
+        Student2 s1 =new Student2();
         s1.id=331;
         s1.name="Tarini";
         System.out.println(s1.id+" "+s1.name);
